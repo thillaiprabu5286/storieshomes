124c7223a066c0a36273c7df559adfcb68afd68d
Thillai Prabu <prabusmiles05@gmail.com>
2017-02-02 18:15:04 +0400

    Add new theme color

--- app/code/local/Themevast/Themevast/Model/System/Config/Source/Themecolor.php
+++ app/code/local/Themevast/Themevast/Model/System/Config/Source/Themecolor.php
@@ -12,6 +12,7 @@ class Themevast_Themevast_Model_System_Config_Source_Themecolor
              array('value'=>'color/blue.css', 'label'=>Mage::helper('adminhtml')->__('Blue')),
              array('value'=>'color/red.css', 'label'=>Mage::helper('adminhtml')->__('Red')),
              array('value'=>'color/gray.css', 'label'=>Mage::helper('adminhtml')->__('Gray')),
+            array('value'=>'color/green.css', 'label'=>Mage::helper('adminhtml')->__('Green')),
         );
         $label = $options ? Mage::helper('core')->__('-- Please Select --') : Mage::helper('core')->__('-- One Color --');
         if ($withEmpty) {
