17eb8948fabfada6a760ad4490a888a1e8a57a71
Thillai Prabu <prabusmiles05@gmail.com>
2017-12-28 10:46:20 +0530

    Format price

--- app/code/local/Dever/Sms/Helper/Data.php
+++ app/code/local/Dever/Sms/Helper/Data.php
@@ -15,7 +15,10 @@ class Dever_Sms_Helper_Data extends Mage_Core_Helper_Abstract
         switch ($type)
         {
             case 'deals':
-                $content = "Dear {$options['name']},\nThank you for your interest in our product: {$options['product_name']} of product code: {$options['sku']} for Rs.{$options['price']}. Your coupon code is {$options['coupon_code']}. Show the coupon at store during the time of purchase. Limited stocks only. Visit Stories, Calicut. Hurry up. T&C Apply.\nRegards,\nTeam Stories";
+                /** @var Mage_Directory_Model_Currency $priceModel */
+                $priceModel = Mage::getModel('directory/currency');
+                $formattedPrice = $priceModel->formatPrecision($options['price'], 0 );
+                $content = "Dear {$options['name']},\nThank you for your interest in our product: {$options['product_name']} of product code: {$options['sku']} for Rs.{$formattedPrice}. Your coupon code is {$options['coupon_code']}. Show the coupon at store during the time of purchase. Limited stocks only. Visit Stories, Calicut. Hurry up. T&C Apply.\nRegards,\nTeam Stories";
                 break;
             default:
                 //Do Nothing
